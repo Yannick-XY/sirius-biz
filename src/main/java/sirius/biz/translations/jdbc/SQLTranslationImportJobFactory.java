c1de47ce18279860f9583275dfce69b8cf09c85a
Christian Schierle <csc@scireum.de>
2020-08-13 16:35:28 +0200

    Adds CodeList parameter
    Fixes: SIRI-243

--- src/main/java/sirius/biz/translations/jdbc/SQLTranslationImportJobFactory.java
+++ src/main/java/sirius/biz/translations/jdbc/SQLTranslationImportJobFactory.java
@@ -9,17 +9,31 @@
 package sirius.biz.translations.jdbc;
 
 import sirius.biz.codelists.CodeListController;
+import sirius.biz.codelists.CodeListEntry;
+import sirius.biz.codelists.jdbc.SQLCodeList;
+import sirius.biz.codelists.jdbc.SQLCodeListEntry;
 import sirius.biz.codelists.jdbc.SQLCodeLists;
+import sirius.biz.importer.ImportContext;
 import sirius.biz.jobs.batch.file.EntityImportJobFactory;
+import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.Parameter;
+import sirius.biz.process.ProcessContext;
 import sirius.db.mixing.BaseEntity;
 import sirius.kernel.di.std.Register;
+import sirius.web.http.QueryString;
 import sirius.web.security.Permission;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.Consumer;
 
+/**
+ * Provides an import job for {@link SQLTranslation translations} of {@link SQLCodeListEntry code list entries} stored in a JDBC database.
+ */
 @Register(framework = SQLCodeLists.FRAMEWORK_CODE_LISTS_JDBC)
 @Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
 public class SQLTranslationImportJobFactory extends EntityImportJobFactory {
+    private CodeListParameter codeListParameter = new CodeListParameter("codeList", "$CodeList").markRequired();
 
     @Override
     protected Class<? extends BaseEntity<?>> getImportType() {
@@ -30,5 +44,27 @@ public class SQLTranslationImportJobFactory extends EntityImportJobFactory {
     @Override
     public String getName() {
         return "import-sql-translations";
+    }
+
+    @Override
+    protected void collectParameters(Consumer<Parameter<?, ?>> parameterCollector) {
+        parameterCollector.accept(codeListParameter);
+        super.collectParameters(parameterCollector);
+    }
+
+    @Override
+    protected void transferParameters(ImportContext context, ProcessContext processContext) {
+        // sets "codelist" -> "$(code)" in context
+        context.set(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+    }
+
+    @Override
+    protected boolean hasPresetFor(QueryString queryString, Object targetObject) {
+        return targetObject instanceof SQLCodeList;
+    }
+
+    @Override
+    protected void computePresetFor(QueryString queryString, Object targetObject, Map<String, Object> preset) {
+        preset.put(codeListParameter.getName(), ((SQLCodeList) targetObject).getCodeListData().getCode());
     }
 }
--- src/main/java/sirius/biz/translations/mongo/MongoTranslationImportJobFactory.java
+++ src/main/java/sirius/biz/translations/mongo/MongoTranslationImportJobFactory.java
@@ -9,17 +9,31 @@
 package sirius.biz.translations.mongo;
 
 import sirius.biz.codelists.CodeListController;
+import sirius.biz.codelists.CodeListEntry;
+import sirius.biz.codelists.jdbc.SQLCodeList;
 import sirius.biz.codelists.mongo.MongoCodeLists;
+import sirius.biz.importer.ImportContext;
 import sirius.biz.jobs.batch.file.EntityImportJobFactory;
+import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.Parameter;
+import sirius.biz.process.ProcessContext;
 import sirius.db.mixing.BaseEntity;
 import sirius.kernel.di.std.Register;
+import sirius.web.http.QueryString;
 import sirius.web.security.Permission;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.Consumer;
 
+/**
+ * Provides an import job for {@link MongoTranslation translations} of {@link sirius.biz.codelists.mongo.MongoCodeListEntry code list entries} stored in a JDBC database.
+ */
 @Register(framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
 @Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
 public class MongoTranslationImportJobFactory extends EntityImportJobFactory {
+    private CodeListParameter codeListParameter = new CodeListParameter("codeList", "$CodeList").markRequired();
+
     @Override
     protected Class<? extends BaseEntity<?>> getImportType() {
         return MongoTranslation.class;
@@ -29,5 +43,27 @@ public class MongoTranslationImportJobFactory extends EntityImportJobFactory {
     @Override
     public String getName() {
         return "import-mongo-translations";
+    }
+
+    @Override
+    protected void collectParameters(Consumer<Parameter<?, ?>> parameterCollector) {
+        parameterCollector.accept(codeListParameter);
+        super.collectParameters(parameterCollector);
+    }
+
+    @Override
+    protected void transferParameters(ImportContext context, ProcessContext processContext) {
+        // sets "codelist" -> "$(code)" in context
+        context.set(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+    }
+
+    @Override
+    protected boolean hasPresetFor(QueryString queryString, Object targetObject) {
+        return targetObject instanceof SQLCodeList;
+    }
+
+    @Override
+    protected void computePresetFor(QueryString queryString, Object targetObject, Map<String, Object> preset) {
+        preset.put(codeListParameter.getName(), ((SQLCodeList) targetObject).getCodeListData().getCode());
     }
 }
